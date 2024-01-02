89c33e3528f337f76f48ffd107e957faa3f12f45
Steven Zack <zigzigcheers@gmail.com>
2024-01-02 09:48:43 +0200

    svelte-7.css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import "$lib/leafapp.css";
+</script>
+
+<div class="c">
+  <div class="cs w maxwsm p">
+    <h3 class="brand">Steven's Blog</h3>
+    <ul role="menubar" class="w">
+      <li role="menuitem" tabindex="0">File</li>
+      <li role="menuitem" tabindex="0">Edit</li>
+      <li role="menuitem" tabindex="0">View</li>
+      <li role="menuitem" tabindex="0">
+        Eng/文
+        <ul role="menu">
+          <li role="menuitem">
+              <a href="/">
+              English 
+            </a>
+          </li>
+          <li role="menuitem">
+            <a href="#">
+              中文 
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <slot />
+  </div>
+</div>
+
+<style>
+  .brand {
+    font-size: 24px;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="window" style="margin: 32px; width: 250px">
+  <div class="title-bar">
+    <div class="title-bar-text">My First Program</div>
+  </div>
+  <div class="window-body p">
+    <p>Hello, world!</p>
+  </div>
+</div>
